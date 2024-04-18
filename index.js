83e64b86181add683cff8987d6aff79b5285f677
retasu1 <liliansungg@gmail.com>
2024-04-18 15:15:49 +0200

    second commit 😬

--- index.js
+++ index.js
@@ -5,25 +5,57 @@ const frame = document.getElementById('bird-list');
 
 data.map((bird) => {
     //console.log(bird);
+
+    function seenClass(bool) {
+        if (bool) {
+            return 'seen'
+        } else {
+            return 'unseen'
+        }
+    }
+
+    function seenEmoji(bool) {
+        if (bool) {
+            return 'seen! ✅'
+        } else {
+            return 'still looking 👀'
+        }
+    }
+
+    function seenIcon(bool) {
+        if (bool) {
+            return '<span class="fluent--checkbox-checked-24-filled"></span>'
+        } else {
+            return '<span class="fluent--checkbox-unchecked-24-regular"></span>'
+        }
+    }
+
     let newDiv = document.createElement("div");
     newDiv.className = "bird";
     newDiv.innerHTML = `
 
+        <h2 class="scientific-name">${bird.Scientific}</h2>
         
         <figure>
             <a href="${bird.photo.photoLink}" target="_blank"><img src="${bird.photo.photoURL}" alt="${bird.English}" class="birdImg"></a>
             <figcaption>Photo by ${bird.photo.photoOwner}, licensed under <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank">CC By 2.0</a></figcaption>
         </figure>
-        <div class="birdNames">
-            <h2>${bird.Dutch}</h2>
-            <h2>${bird.Chinese}</h2>
-            <h2>${bird.English}</h2>
-            
-            <p><i>${bird.Scientific}</i></p>
+
+        <div class="birdNameList">
+            <div class="birdName">
+                ${seenIcon(bird.seenIn.nl)}
+                <h3>🇳🇱 ${bird.Dutch}</h3>
+            </div>
+            <div class="birdName">
+                ${seenIcon(bird.seenIn.tw)}
+                <h3>🇹🇼 ${bird.Chinese}</h3>
+            </div>
+            <div class="birdName">
+                ${seenIcon(bird.seenIn.else)}
+                <h3>🗺️ ${bird.English}</h3>
+            </div>
         </div>
         
     `;
     frame.appendChild(newDiv)
 })
-
-const apiKey = process.env.API_KEY;
